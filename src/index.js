df02ae2d8bd2d483d86ef192a5e2b9e32ad68306
Zaahid <zaahid0180@gmail.com>
2024-10-18 19:32:07 +0530

    initial saving shopping

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ShoppingCart from './ShoppingCart';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ShoppingCart/>
+  </React.StrictMode>
+);
